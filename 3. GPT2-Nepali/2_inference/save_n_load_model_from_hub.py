5516eb85b69f0af3aaceecd2dbae1c4d8feea624
aananda-giri <aanandaprashadgiri@gmail.com>
2025-02-01 10:22:55 +0545

    udpate generate function

--- 3. GPT2-Nepali/2_inference/save_n_load_model_from_hub.py
+++ 3. GPT2-Nepali/2_inference/save_n_load_model_from_hub.py
@@ -1,11 +1,12 @@
 from transformers import PreTrainedTokenizerFast
 import torch
 import torch.nn as nn
-from model_code import GPTModel, GPT_CONFIG_124M, generate_and_print_sample
+from model_code import GPTModel, GPT_CONFIG_124M, generate
 
 
-# load the model
-# ----------------------------
+# --------------------------------------
+# 1. load the model fom local directory
+# --------------------------------------
 
 model = GPTModel(GPT_CONFIG_124M)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -24,15 +25,24 @@ tokenizer = PreTrainedTokenizerFast.from_pretrained("Aananda-giri/NepaliBPE")
 
 # generate a sample
 # ----------------------------
-start_context = "रामले भात"
+prompt = "रामले भात"
 
-generate_and_print_sample(
-    model, tokenizer, device, start_context
-    )
+generate(
+    model,
+    prompt,
+    tokenizer,
+    max_new_tokens=100,
+    temperature=0.7,
+    top_k=50,
+    top_p=None,  # New parameter for nucleus sampling
+    eos_id=None,
+    repetition_penalty=1.2,
+    penalize_len_below=50
+)
 
 
-
-# push model to huggingface
+# ----------------------------
+# 2. push model to huggingface
 # ----------------------------
 # ## import os
 import os
@@ -44,19 +54,48 @@ hf_token = user_secrets.get_secret("HF_TOKEN")
 model.push_to_hub("Aananda-giri/GPT2-Nepali", token=hf_token)
 
 
+# ---------------------------------
 # reload model from huggingface
+# ---------------------------------
+from transformers import PreTrainedTokenizerFast
+import torch
+import torch.nn as nn
+from gpt_model import GPTModel, GPT_CONFIG_124M, generate
+
+
+# load the model
 # ----------------------------
-from previous import load_model_n_tokenizer, generate
-model, tokenizer = load_model_n_tokenizer()
+
+model = GPTModel(GPT_CONFIG_124M)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+# checkpoint = torch.load('/kaggle/input/sebastian-v4/model_checkpoints/model_pg_190000_steps.pth', weights_only=False)
+# # modified (added model loading code)
+# model.load_state_dict(checkpoint["model_state_dict"])
+
+model = GPTModel.from_pretrained("Aananda-giri/GPT2-Nepali")
+model.to(device)
+
+# load the tokenizer
+# ----------------------------
+# tokenizer = PreTrainedTokenizerFast.from_pretrained("Aananda-giri/NepaliBPE")
+
+
+# generate a sample
+# ----------------------------
 
 prompt = "रामले भात"
 
-text = generate(  # function uses `with torch.no_grad()` internally already
-        model=loaded_model,
-        prompt=prompt,
-        tokenizer=tokenizer,
-        max_new_tokens=50,
-        top_k=3,
-        temperature=3.0
-    )
-text
+generate(
+    model,
+    prompt,
+    tokenizer,
+    max_new_tokens=100,
+    temperature=0.7,
+    top_k=50,
+    top_p=None,  # New parameter for nucleus sampling
+    eos_id=None,
+    repetition_penalty=1.2,
+    penalize_len_below=50
+)
